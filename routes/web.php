f828f284227b8abde28a8469108fd7bcde4136da
Adeyemi <adeyemitimmy@gmail.com>
2022-02-23 09:47:29 +0100

    first stage

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Dcblogdev\Xero\Facades\Xero;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['web', 'XeroAuthenticated']], function(){
+    Route::get('xero', function(){
+        return Xero::get('Invoices');
+    });
+});
+Route::get('xero/connect', function(){
+    return Xero::connect();
+});
